21409019da35d202348dcf4e47767f41c98c43d9
Casper van Genderingen <c.vangenderingen@hotmail.com>
2023-05-23 12:33:53 +0200

    Clean up

--- src/main/java/io/swagger/api/UsersApi.java
+++ src/main/java/io/swagger/api/UsersApi.java
@@ -5,10 +5,10 @@
  */
 package io.swagger.api;
 
-import io.swagger.model.CreateUserDTO;
-import io.swagger.model.GetUserDTO;
+import io.swagger.model.DTO.CreateUserDTO;
+import io.swagger.model.DTO.GetUserDTO;
 import java.util.UUID;
-import io.swagger.model.UpdateUserDTO;
+import io.swagger.model.DTO.UpdateUserDTO;
 import io.swagger.model.User;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -23,18 +23,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.bind.annotation.CookieValue;
 
 import javax.validation.Valid;
 import javax.validation.constraints.*;
 import java.util.List;
-import java.util.Map;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2023-05-16T13:11:00.686570329Z[GMT]")
 @Validated
