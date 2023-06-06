2da15f5295e015a7c1fe23087e9c8ce5be6b5b36
Casper van Genderingen <39115316+vanGenderingen@users.noreply.github.com>
2023-06-06 12:39:08 +0200

    Transactions into main (#10)
    * WIP filters
    
    * WIP transaction filters
    
    * fixed some filters

---
+++ src/main/java/io/swagger/api/repository/AccountRepository.java
@@ -0,0 +1,23 @@
+package io.swagger.api.repository;
+
+import io.swagger.model.Account;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Account getAccountByAccountID(UUID accountID);
+    @Query("SELECT a FROM Account a WHERE a.user.userID = :userId AND (:searchStrings IS NULL OR LOWER(a.name) LIKE CONCAT('%', LOWER(:searchStrings), '%'))")
+    Page<Account> getAccountsOfUser(
+            @Param("userId") UUID userId,
+            @Param("searchStrings") String searchStrings,
+            Pageable pageable
+    );
+}
