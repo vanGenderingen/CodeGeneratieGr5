a36121f4411b582692b254d35879872a88080a93
Casper van Genderingen <c.vangenderingen@hotmail.com>
2023-06-08 16:45:15 +0200

    Wip data seeder

--- src/main/java/io/swagger/configuration/DataSeeder.java
+++ src/main/java/io/swagger/configuration/DataSeeder.java
@@ -34,24 +34,20 @@ public class DataSeeder implements ApplicationRunner {
         objectMapper = new ObjectMapper();
         User bank = new User(UUID.fromString("03259d6f-4c93-40f0-b18d-be04f42537fc"), "bank", "bank", "debank@mail.nl", "password", User.RoleEnum.EMPLOYEE, true, new ArrayList<>(), 99999999999.00, 999999999999.00);
         userService.add(bank);
+        User bank2 = userService.getUserByEmail("debank@mail.nl");
 
-        
-
-//        Account bankAccount = new Account(UUID.randomUUID(), bank, UUID.fromString("03259d6f-4c93-40f0-b18d-be04f42537fc"), "Bank account", "NL01INHO0000000001" ,9999999999999999.00, Account.TypeEnum.CURRENT, -9999999999999999.00, true);
-        CreateAccountDTO createAccountDTO1 = new CreateAccountDTO(bank.getUserID(), "Bank", 9999999999999999.00, CreateAccountDTO.TypeEnum.CURRENT, -9999999999999999.00);
-        Account bankAccount = objectMapper.convertValue(createAccountDTO1, Account.class);
-
+        Account bankAccount = new Account(UUID.randomUUID(), bank2, bank2.getUserID(), "Bank account", "NL01INHO0000000001" ,9999999999999999.00, Account.TypeEnum.CURRENT, -9999999999999999.00, true);
         accountService.add(bankAccount);
-
 
         User johnDoeUser = new User(UUID.fromString("bb0cc36d-69a7-471e-a665-3609bc14c27a"), "john", "doe", "john.doe@mail.nl", "password", User.RoleEnum.USER, true, new ArrayList<>(), 1000.00, 10000.00);
         userService.add(johnDoeUser);
+        User johnDoeUser2 = userService.getUserByEmail("john.doe@mail.nl");
 
-        CreateAccountDTO createAccountJohnDoe = new CreateAccountDTO(johnDoeUser.getUserID(), "Current account John Doe", 100.00, CreateAccountDTO.TypeEnum.CURRENT, 0.00);
+        CreateAccountDTO createAccountJohnDoe = new CreateAccountDTO(johnDoeUser2.getUserID(), "Current account John Doe", 100.00, CreateAccountDTO.TypeEnum.CURRENT, 0.00);
         Account accountJohnDoe = objectMapper.convertValue(createAccountJohnDoe, Account.class);
         accountService.add(accountJohnDoe);
 
-        CreateAccountDTO createSavingAccountJohnDoe = new CreateAccountDTO(johnDoeUser.getUserID(), "Saving account John Doe", 100.00, CreateAccountDTO.TypeEnum.SAVINGS, 0.00);
+        CreateAccountDTO createSavingAccountJohnDoe = new CreateAccountDTO(johnDoeUser2.getUserID(), "Saving account John Doe", 100.00, CreateAccountDTO.TypeEnum.SAVINGS, 0.00);
         Account savingAccountJohnDoe = objectMapper.convertValue(createSavingAccountJohnDoe, Account.class);
         accountService.add(savingAccountJohnDoe);
 
