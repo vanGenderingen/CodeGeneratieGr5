466d38fed9ece148bcb44be1cedecd05f3cc806c
666987 <70690069+666987@users.noreply.github.com>
2023-06-08 17:27:53 +0200

    added crossorigin

--- src/main/java/io/swagger/api/controllers/UsersApiController.java
+++ src/main/java/io/swagger/api/controllers/UsersApiController.java
@@ -26,6 +26,7 @@ import java.util.List;
 import java.util.UUID;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2023-05-16T13:11:00.686570329Z[GMT]")
+@CrossOrigin(origins = "*")
 @RestController
 public class UsersApiController implements UsersApi {
 
