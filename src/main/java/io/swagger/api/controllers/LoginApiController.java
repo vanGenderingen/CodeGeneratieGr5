92a0c36504c52cc66ffb491398cbef37f38f1e6e
Jelle Koomen <668419@student.inholland.nl>
2023-06-09 21:37:10 +0200

    update

--- src/main/java/io/swagger/api/controllers/LoginApiController.java
+++ src/main/java/io/swagger/api/controllers/LoginApiController.java
@@ -15,15 +15,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2023-05-16T13:11:00.686570329Z[GMT]")
+@CrossOrigin(origins = "*")
 @RestController
 public class LoginApiController implements LoginApi {
 
