9c0182cf1b5991bdc2cbaa9389e75c71f57dd24c
666987 <70690069+666987@users.noreply.github.com>
2023-06-15 22:25:07 +0200

    Update UsersApiTest.java

--- src/tests/io/swagger/api/UsersApiTest.java
+++ src/tests/io/swagger/api/UsersApiTest.java
@@ -1,26 +1,22 @@
 package io.swagger.api;
 
 import io.swagger.api.controllers.UsersApiController;
-import io.swagger.api.service.UserService;
-import io.swagger.model.Account;
 import io.swagger.model.DTO.CreateUserDTO;
 import io.swagger.model.DTO.GetUserDTO;
 import io.swagger.model.DTO.UpdateUserDTO;
 import io.swagger.model.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.security.Principal;
-import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 class UsersApiTest {
@@ -42,50 +38,120 @@ class UsersApiTest {
         Assertions.assertEquals(HttpStatus.CREATED, response.getStatusCode());
         User user = response.getBody();
     }
-    /*
+
+    @Test
+    void createUser_ReturnsBadRequest() {
+        CreateUserDTO createUserDTO = new CreateUserDTO();
+
+        when(usersApiController.createUser(createUserDTO)).thenReturn(new ResponseEntity<User>(HttpStatus.BAD_REQUEST));
+
+        ResponseEntity<User> response = usersApiController.createUser(createUserDTO);
+
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
     @Test
     void getUsers_shouldReturnListOfUsers() {
         Integer limit = 10;
         Integer offset = 0;
-        String searchStrings = "search";
-        String email = "example@example.com";
-        List<GetUserDTO> users = Collections.singletonList(new GetUserDTO());
-        ResponseEntity<List<GetUserDTO>> expectedResponse = ResponseEntity.ok(users);
+        String searchStrings = "";
+        String email = "";
 
-        when(userService.getAllUsers(limit, offset, searchStrings, email)).thenReturn(users);
+        // Set the mock behaviour of the usersApiController
+        when(usersApiController.getUsers(limit, offset, searchStrings, email)).thenReturn(new ResponseEntity<List<GetUserDTO>>(HttpStatus.OK));
 
-        ResponseEntity<List<GetUserDTO>> response = usersApi.getUsers(limit, offset, searchStrings, email);
+        // Call the usersGet method
+        ResponseEntity<List<GetUserDTO>> response = usersApiController.getUsers(limit, offset, searchStrings, email);
 
-        assertEquals(expectedResponse, response);
+        // Verify the response
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        List<GetUserDTO> users = response.getBody();
+    }
+
+    @Test
+    void getUsers_ReturnsNotFound() {
+        Integer limit = 10;
+        Integer offset = 0;
+        String searchStrings = "";
+        String email = "";
+
+        // Set the mock behaviour of the usersApiController
+        when(usersApiController.getUsers(limit, offset, searchStrings, email)).thenReturn(new ResponseEntity<List<GetUserDTO>>(HttpStatus.NOT_FOUND));
+
+        // Call the usersGet method
+        ResponseEntity<List<GetUserDTO>> response = usersApiController.getUsers(limit, offset, searchStrings, email);
+
+        // Verify the response
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 
     @Test
     void getUserById_shouldReturnUser() {
+        // Create test data
         UUID userId = UUID.randomUUID();
-        GetUserDTO user = new GetUserDTO();
-        Principal principal = () -> "username";
-        ResponseEntity<GetUserDTO> expectedResponse = ResponseEntity.ok(user);
+        Principal principal = Mockito.mock(Principal.class);
 
-        when(userService.getUserByUserID(userId)).thenReturn(user);
+        // Set the mock behaviour of the usersApiController
+        when(usersApiController.getUserById(userId, principal)).thenReturn(new ResponseEntity<GetUserDTO>(HttpStatus.OK));
 
-        ResponseEntity<GetUserDTO> response = usersApi.getUserById(userId, principal);
+        // Call the usersGet method
+        ResponseEntity<GetUserDTO> response = usersApiController.getUserById(userId, principal);
 
-        assertEquals(expectedResponse, response);
+        // Verify the response
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        GetUserDTO user = response.getBody();
+    }
+
+    @Test
+    void getUserById_ReturnsNotFound() {
+        // Create test data
+        UUID userId = UUID.randomUUID();
+        Principal principal = Mockito.mock(Principal.class);
+
+        // Set the mock behaviour of the usersApiController
+        when(usersApiController.getUserById(userId, principal)).thenReturn(new ResponseEntity<GetUserDTO>(HttpStatus.NOT_FOUND));
+
+        // Call the usersGet method
+        ResponseEntity<GetUserDTO> response = usersApiController.getUserById(userId, principal);
+
+        // Verify the response
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 
     @Test
     void updateUser_shouldReturnUpdatedUser() {
+        // Create test data
         UUID userId = UUID.randomUUID();
         UpdateUserDTO updateUserDTO = new UpdateUserDTO();
-        GetUserDTO updatedUser = new GetUserDTO();
-        Principal principal = () -> "username";
-        ResponseEntity<GetUserDTO> expectedResponse = ResponseEntity.ok(updatedUser);
+        Principal principal = Mockito.mock(Principal.class);
 
-        when(userService.updateUser(userId, updateUserDTO)).thenReturn(updatedUser);
+        // Set the mock behaviour of the usersApiController
+        when(usersApiController.updateUser(userId, updateUserDTO, principal)).thenReturn(new ResponseEntity<GetUserDTO>(HttpStatus.OK));
 
-        ResponseEntity<GetUserDTO> response = usersApi.updateUser(userId, updateUserDTO, principal);
+        // Call the usersGet method
+        ResponseEntity<GetUserDTO> response = usersApiController.updateUser(userId, updateUserDTO, principal);
 
-        assertEquals(expectedResponse, response);
+        // Verify the response
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        GetUserDTO user = response.getBody();
+
     }
-    */
+
+    @Test
+    void updateUser_ReturnsBadRequest() {
+        // Create test data
+        UUID userId = UUID.randomUUID();
+        UpdateUserDTO updateUserDTO = new UpdateUserDTO();
+        Principal principal = Mockito.mock(Principal.class);
+
+        // Set the mock behaviour of the usersApiController
+        when(usersApiController.updateUser(userId, updateUserDTO, principal)).thenReturn(new ResponseEntity<GetUserDTO>(HttpStatus.BAD_REQUEST));
+
+        // Call the usersGet method
+        ResponseEntity<GetUserDTO> response = usersApiController.updateUser(userId, updateUserDTO, principal);
+
+        // Verify the response
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
 }
